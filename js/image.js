93ee66f0f8e48619e78f3a503874bdf20c16ca14
jchatba <jchatba@gmail.com>
2023-06-26 20:17:31 +0200

    PEACE

--- js/image.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function() {
-    var images = document.getElementsByClassName('gallery img');
-    for (var i = 0; i < images.length; i++) {
-        images[i].style.width = '200px';
-        images[i].style.height = '150px';
-    }
-});
