8ec23fd1362a59c92ca3ff3dd88b7b042ce8919b
jchatba <jchatba@gmail.com>
2023-06-26 20:10:16 +0200

    EDIT

--- js/image.js
+++ js/image.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', function() {
-    var images = document.getElementsByClassName('image-gallery');
+    var images = document.getElementsByClassName('gallery img');
     for (var i = 0; i < images.length; i++) {
         images[i].style.width = '200px';
         images[i].style.height = '150px';
